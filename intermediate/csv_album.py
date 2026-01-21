aee1d490897d281b6fd6e0730715d56f54af7abf
ridhoaffandi <affandiridho@gmail.com>
2026-01-22 05:20:56 +0700

    Add files via upload

---
+++ intermediate/csv_album.py
@@ -0,0 +1,155 @@
+import pandas as pd
+import psycopg2
+from psycopg2.extras import execute_values
+
+SOURCE_DB_CONFIG = {
+    "host": "10.140.159.152",
+    "port": 5432,
+    "dbname": "stagingdb",
+    "user": "admindb",
+    "password": "admin"
+}
+CSV_OUTPUT_PATH = "csv_album.csv"
+
+SOURCE_SQL = """
+select distinct
+	mv."USER_ID",
+	mv."ID",
+	mv."CREATED_TIME",
+	mv."MEDIA_TYPE",
+	mv."MEDIA_PRODUCT_TYPE",
+	mi."LIKE_COUNT",
+	mi."COMMENT_COUNT",
+	mi."CAROUSEL_ALBUM_VIDEO_VIEWS",
+	mi."CAROUSEL_ALBUM_VIEWS",
+	mi."CAROUSEL_ALBUM_SAVED" ,
+	mi."CAROUSEL_ALBUM_SHARES",
+	mi."CAROUSEL_ALBUM_IMPRESSIONS",
+	mi."CAROUSEL_ALBUM_REACH" ,
+	mi."CAROUSEL_ALBUM_ENGAGEMENT",
+	mi."TIMESTAMP"
+from
+(select distinct
+	"USER_ID" ,
+	"ID" ,
+	"MEDIA_TYPE",
+	"MEDIA_PRODUCT_TYPE",
+	"CREATED_TIME"  
+from media_history
+where
+"MEDIA_TYPE"  = 'CAROUSEL_ALBUM' 
+) as mv
+left outer join
+(select distinct
+	"ID",
+	"LIKE_COUNT",
+	"COMMENT_COUNT",
+	"CAROUSEL_ALBUM_VIDEO_VIEWS",
+	"CAROUSEL_ALBUM_VIEWS",
+	"CAROUSEL_ALBUM_SAVED" ,
+	"CAROUSEL_ALBUM_SHARES",
+	"CAROUSEL_ALBUM_IMPRESSIONS",
+	"CAROUSEL_ALBUM_REACH" ,
+	"CAROUSEL_ALBUM_ENGAGEMENT",
+	"TIMESTAMP"
+from media_insights
+where
+"CAROUSEL_ALBUM_VIEWS" is not null ) as mi
+on mv."ID" = mi."ID"
+order by mv."USER_ID" ,mv."ID"  asc
+"""
+
+# =========================
+# FUNCTION: SOURCE QUERY → CSV
+# =========================
+def extract_query_to_csv(sql, csv_path, db_config):
+    conn = psycopg2.connect(**db_config)
+    df = pd.read_sql(sql, conn)
+    conn.close()
+
+    if df.empty:
+        print("Query source tidak menghasilkan data.")
+        return False
+
+    df.to_csv(csv_path, index=False)
+    print(f"Extract {len(df)} rows ke {csv_path}")
+    return True
+
+TARGET_DB_CONFIG = {
+    "host": "10.140.159.152",
+    "port": 5432,
+    "dbname": "intermediatedb",
+    "user": "admindb",
+    "password": "admin"
+}
+INTERMEDIATE_TABLE = "insta_album"
+
+def upload_csv_to_postgres(csv_path, table_name, db_config):
+    df = pd.read_csv(
+        csv_path,
+        dtype=str,
+        keep_default_na=False)
+
+    df = df.replace(
+        to_replace=r"^\s*(nan|NULL|null|NaN|NAN)?\s*$",
+        value=None,
+        regex=True
+    )
+
+    df = df.where(pd.notnull(df), None)
+
+    if df.empty:
+        print("CSV kosong, skip upload.")
+        return
+
+    records = [tuple(row) for row in df.itertuples(index=False, name=None)]
+    columns = ",".join(df.columns)
+
+    insert_query = f"""
+       INSERT INTO insta_album (
+            "USER_ID",
+	        "ID",
+	        "CREATED_TIME",
+	        "MEDIA_TYPE",
+	        "MEDIA_PRODUCT_TYPE",
+	        "LIKE_COUNT",
+	        "COMMENT_COUNT",
+	        "CAROUSEL_ALBUM_VIDEO_VIEWS",
+	        "CAROUSEL_ALBUM_VIEWS",
+	        "CAROUSEL_ALBUM_SAVED" ,
+	        "CAROUSEL_ALBUM_SHARES",
+	        "CAROUSEL_ALBUM_IMPRESSIONS",
+	        "CAROUSEL_ALBUM_REACH" ,
+	        "CAROUSEL_ALBUM_ENGAGEMENT",
+	        "TIMESTAMP")
+            VALUES %s
+    """
+
+    conn = psycopg2.connect(**db_config)
+    cursor = conn.cursor()
+
+    cursor.execute("TRUNCATE TABLE insta_album")
+    execute_values(cursor, insert_query, records)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    print(f"Upload {len(records)} rows ke tabel {table_name} berhasil")
+
+
+# =========================
+# MAIN PIPELINE
+# =========================
+if __name__ == "__main__":
+    success = extract_query_to_csv(
+        sql=SOURCE_SQL,
+        csv_path=CSV_OUTPUT_PATH,
+        db_config=SOURCE_DB_CONFIG
+    )
+    if success:
+        upload_csv_to_postgres(
+            csv_path=CSV_OUTPUT_PATH,
+            table_name=INTERMEDIATE_TABLE,
+            db_config=TARGET_DB_CONFIG
+        )
